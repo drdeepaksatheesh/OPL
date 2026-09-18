d0e5b2c515583b39aba421cbf7523d786f95f804
Dr Deepak S <drdeepaksatheesh@gmail.com>
2026-09-18 09:20:19 +0530

    site: add runtime configuration

---
+++ site/config.js
@@ -0,0 +1,9 @@
+window.OPL_CONFIG = Object.freeze({
+  version: "0.1-reference-dev",
+  analytics: {
+    enabled: false,
+    provider: "none",
+    countEndpoint: "",
+    publicCounterJson: ""
+  }
+});
